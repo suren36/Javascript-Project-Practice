230a1ad9bb495743a919822b6b2d2ca95e7fbc6e
suren36 <79780614+suren36@users.noreply.github.com>
2023-11-28 10:26:17 +0545

    jsProject

---
+++ day-39 strongPassword/script.js
@@ -0,0 +1,9 @@
+const background = document.querySelector(".background");
+const password = document.querySelector("#password");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  const length = input.length;
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});

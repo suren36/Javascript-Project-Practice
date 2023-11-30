9c51db0ad163645b59bd434f7c22885fd9142e67
suren36 <79780614+suren36@users.noreply.github.com>
2023-11-30 11:41:01 +0545

    JSProject

---
+++ day-41 Verify Account UI/script.js
@@ -0,0 +1,22 @@
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+      codes[idx].value = "";
+      setTimeout(() => {
+        if (codes[idx + 1]) {
+          codes[idx + 1].focus();
+        }
+      }, 10);
+    } else if (e.key === "Backspace") {
+      setTimeout(() => {
+        if (codes[idx - 1]) {
+          codes[idx - 1].focus();
+        }
+      }, 10);
+    }
+  });
+});

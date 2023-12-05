2182214d951f3be6363124650a7cd5bcb63fb34d
suren36 <79780614+suren36@users.noreply.github.com>
2023-12-05 20:17:50 +0545

    jsProject

---
+++ day-43 Feedback Ui design/script.js
@@ -0,0 +1,42 @@
+const rating = document.querySelectorAll(".rating");
+const sendBtn = document.getElementById("send");
+const panel = document.getElementById("panel");
+
+getData();
+
+filter.addEventListener("input", (e) => {
+  filterData(e.target.value);
+});
+async function getData() {
+  const res = await fetch("https://randomuser.me/api?results=50");
+
+  const { results } = await res.json();
+
+  result.innerHTML = ""; //clear Results
+
+  results.forEach((user) => {
+    const li = document.createElement("li");
+
+    listitems.push(li);
+
+    li.innerHTML = `
+    <img src="${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+      <h4>${user.name.first} ${user.name.last}</h4>
+      <p>${user.location.city}, ${user.location.country}</p>
+    </div>
+  `;
+
+    result.appendChild(li);
+  });
+}
+
+function filterData(searchTerm) {
+  listitems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
+}
